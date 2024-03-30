4727970e642b41e72c57d31d75f9b6877250257e
Daniel Junior <jrzabott@gmail.com>
2024-03-30 17:48:36 +0000

    e-folio de sistemas operativos A. Rascunho. Fix comments.

--- eF-A-SO/fup.c
+++ eF-A-SO/fup.c
@@ -1,7 +1,7 @@
 /*
 ** UC: 21111 - Sistemas Operativos
-** e-flio A 2023-24 (fdup.c)
-** Aluno: 2304335 - Daniel Junior
+** e-fólio A 2023-24 (fdup.c)
+** Aluno: 2304335 - Daniel Júnior
 */
 
 #include <stdio.h>
@@ -12,7 +12,7 @@
 #include <string.h>
 
 // Função para verificar a existência do diretório e a validade dos argumentos passados ao programa.
-// Garante que o número correto de argumentos seja fornecido e que o diretório exista.
+// Garante que o número correto de argumentos seja fornecido e que o diretório especificado exista.
 void checkArguments(int argc, char* argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Erro: Utilização incorreta. Forma correta: %s <diretório> <padrão do ficheiro>\n", argv[0]);
@@ -20,7 +20,7 @@ void checkArguments(int argc, char* argv[]) {
     }
 
     struct stat statbuf;
-    // Verifica se o diretório existe e se é realmente um diretório.
+    // Verifica se o diretório especificado existe e é de fato um diretório.
     if (stat(argv[1], &statbuf) != 0 || !S_ISDIR(statbuf.st_mode)) {
         fprintf(stderr, "Erro: O diretório '%s' é inválido ou não existe.\n", argv[1]);
         exit(EXIT_FAILURE);
@@ -34,14 +34,14 @@ void executeCommand(char* command[], const char* outputFile) {
 
     if (pid == 0) { // Processo filho
         if (outputFile != NULL) {
-            // Redireciona a saída padrão para o arquivo especificado.
+            // Redireciona a saída padrão para o ficheiro especificado.
             if (freopen(outputFile, "w", stdout) == NULL) {
-                fprintf(stderr, "Erro ao redirecionar a saída para o arquivo %s\n", outputFile);
+                fprintf(stderr, "Erro ao redirecionar a saída para o ficheiro %s\n", outputFile);
                 exit(EXIT_FAILURE);
             }
         }
         execvp(command[0], command);
-        // Caso execvp retorne, indica que ocorreu um erro na execução do comando.
+        // Caso execvp retorne, houve um erro na execução do comando.
         fprintf(stderr, "Erro ao executar o comando %s\n", command[0]);
         exit(EXIT_FAILURE);
     } else if (pid > 0) { // Processo pai
@@ -51,34 +51,34 @@ void executeCommand(char* command[], const char* outputFile) {
             fprintf(stderr, "Erro: O comando %s falhou na execução.\n", command[0]);
         }
     } else {
-        // Caso a criação do processo filho falhe.
+        // Em caso de falha ao criar um novo processo.
         fprintf(stderr, "Erro ao criar um novo processo com fork().\n");
         exit(EXIT_FAILURE);
     }
 }
 
-// Funo para a execuo do comando find.
+// Função para a execução do comando find.
 void findFiles(char* directory, char* pattern) {
     char* command[] = {"find", directory, "-type", "f", "-name", pattern, "-printf", "%h %f\n", NULL};
     executeCommand(command, "tmp1.txt");
 }
 
-// Funo para a execuo do comando sort.
+// Função para a execução do comando sort.
 void sortFiles() {
     char* command[] = {"sort", "-t", " ", "-k2,2", "tmp1.txt", NULL};
     executeCommand(command, "tmp2.txt");
 }
 
-// Funo para a execuo do comando uniq.
+// Função para a execução do comando uniq.
 void uniqFiles() {
     char* command[] = {"uniq", "-f1", "-D", "tmp2.txt", NULL};
     executeCommand(command, "tmp3.txt");
 }
 
-// Funo para a execuo do comando awk.
+// Função para a execução do comando awk.
 void formatOutput() {
     char* command[] = {"awk", "{print $2,$1}", "tmp3.txt", NULL};
-    executeCommand(command, NULL); // A sada ser direcionada para a sada padro.
+    executeCommand(command, NULL); // A saída será direcionada para a saída padrão.
 }
 
 int main(int argc, char* argv[]) {
