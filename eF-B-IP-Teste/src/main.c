ebb3d3a931cd0da5f4ba692b96ae2171eb030107
Daniel Junior <jrzabott@gmail.com>
2025-01-06 17:02:41 +0000

    correcao de bugs.. estava a nao liberar ponteiros (memoery leaks)

--- eF-B-IP-Teste/src/main.c
+++ eF-B-IP-Teste/src/main.c
@@ -1,21 +1,34 @@
 #include <TipoVeiculo.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "RegistoSensor.h"
 
 int main() {
+    char *tipo_veiculo_to_string = TipoVeiculoToString(LIGEIRO);
     // will print all vehicle types for testing
-    printf("Ligeiro: ['%c'] ['%s']\n", getTipoVeiculo(LIGEIRO), TipoVeiculoToString(LIGEIRO));
-    printf("Pesado: ['%c'] ['%s']\n", getTipoVeiculo(PESADO), TipoVeiculoToString(PESADO));
-    printf("Motociclo: ['%c'] ['%s']\n", getTipoVeiculo(MOTOCICLO), TipoVeiculoToString(MOTOCICLO));
+    printf("Ligeiro: ['%c'] ['%s']\n", getTipoVeiculo(LIGEIRO), tipo_veiculo_to_string);
+    free(tipo_veiculo_to_string);
+
+    tipo_veiculo_to_string = TipoVeiculoToString(PESADO);
+    printf("Pesado: ['%c'] ['%s']\n", getTipoVeiculo(PESADO), tipo_veiculo_to_string);
+    free(tipo_veiculo_to_string);
+
+    tipo_veiculo_to_string = TipoVeiculoToString(MOTOCICLO);
+    printf("Motociclo: ['%c'] ['%s']\n", getTipoVeiculo(MOTOCICLO), tipo_veiculo_to_string);
+    free(tipo_veiculo_to_string);
 
     // test some unsupported vehicle type
-    printf("Desconhecido: ['%c'] ['%s']\n", getTipoVeiculo(DESCONHECIDO), TipoVeiculoToString(DESCONHECIDO));
+    tipo_veiculo_to_string = TipoVeiculoToString(DESCONHECIDO);
+    printf("Desconhecido: ['%c'] ['%s']\n", getTipoVeiculo(DESCONHECIDO), tipo_veiculo_to_string);
+    free(tipo_veiculo_to_string);
 
     printf("\n\n=================================================\n");
 
     // Criar um novo RegistoSensor e depois imprimir no terminal o resultado
     RegistoSensor registo = {"00-00-00", LIGEIRO, 100, "A1", "Lisboa"};
-    printf("%s", RegistoSensorToString(registo));
+    char *registo_sensor_to_string = RegistoSensorToString(registo);
+    printf("%s", registo_sensor_to_string);
+    free(registo_sensor_to_string);
     return 0;
 }
